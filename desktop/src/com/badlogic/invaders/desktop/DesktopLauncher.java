7d38b22831b29fe3339a945b74343d3548f98dc3
David Young-Chan Kay <davidykay@gmail.com>
2014-11-08 06:57:11 -0500

    made desktop launcher work.

--- desktop/src/com/badlogic/invaders/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/invaders/desktop/DesktopLauncher.java
@@ -7,9 +7,10 @@ import com.badlogic.invaders.Invaders;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 480;
-		config.height = 320;
-		config.title = "Invaders";
+		// config.width = 480;
+		// config.height = 320;
+		// config.title = "Invaders";
+
 		new LwjglApplication(new Invaders(), config);
 	}
 }
